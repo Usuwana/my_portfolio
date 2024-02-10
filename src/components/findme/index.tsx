fdf5c94e1cb895184ecf6bd8c95220f910c5ba05
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-11 04:51:53 +1100

    .

--- src/components/findme/index.tsx
+++ src/components/findme/index.tsx
@@ -1,7 +1,18 @@
+import github from './/github.png';
+import linkedin from './/linkedin.png';
+import dev from './/dev.png';
+import hashnode from './/hashnode.png'
+
 export const FindMe  = () => {
     return (
-        <div style={{ backgroundColor: '#333', color: 'aliceblue', textAlign: 'center', width: '100%', height: '100%' }}>
+        <div style={{ backgroundColor: '#1C1A1A', color: 'aliceblue', textAlign: 'center', width: '100%', height: '100%' }}>
             <span>You can find me on:</span>
+            <div style={{display: 'flex', justifyContent: 'center', paddingTop: '50px', paddingBottom: '50px', height: '100%' }}>
+                <img src={github} style={{ width: '40px', height: '40px', marginRight: '100px' }}/>
+                <img src={linkedin} style={{ width: '40px', height: '40px',  marginRight: '100px'  }}/>
+                <img src={dev} style={{ width: '40px', height: '40px',  marginRight: '100px'  }}/>
+                <img src={hashnode} style={{ width: '40px', height: '40px' }}/>
+            </div>
         </div>
     )
 }
