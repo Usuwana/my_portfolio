07ef04dbfb4e2b73167669b955492ee1255e6976
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-13 01:54:03 +1100

    .

--- src/screens/projects/index.tsx
+++ src/screens/projects/index.tsx
@@ -1,5 +1,11 @@
+import { Footer } from "../../components/footer"
+import { Header } from "../../components/header"
+
 export const Projects  = () => {
     return (
-        <div></div>
+        <div style={{height: '100vh', width: '100vw', backgroundColor: '#1C1A1A'}}>
+            <Header/>
+            <Footer/>
+        </div>
     )
 }
