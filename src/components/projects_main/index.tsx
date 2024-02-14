99184179a39dfc339175e8c7ba96da69b739d843
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-15 00:38:59 +1100

    .

--- src/components/projects_main/index.tsx
+++ src/components/projects_main/index.tsx
@@ -1,17 +1,18 @@
 import './styles.css';
 import red from './/red.png';
 import ac from './/ac.png';
+import realfun from './/realfun.png';
 import Spacer from 'react-styled-spacer';
 
 export const ProjectsMain = () => {
     return(
-<div>
+<div style={{backgroundColor: '#1C1A1A'}}>
 <Spacer h={30} />
 <div className="rowContainer">
       <div className="column">
         {/* Content for the first column */}
         <h4>RuesEvents Designs</h4>
-        <img src={red} style={{display: 'block', width: '750px', height: '500px', margin: 'auto' }}/>
+        <a href="http://ruesevents.co.zw/" target="_blank"><img src={red} style={{display: 'block', width: '750px', height: '500px', margin: 'auto' }}/></a>
       </div>
       <div className="column">
         {/* Content for the second column */}
@@ -23,7 +24,20 @@ export const ProjectsMain = () => {
       <div className="column">
         {/* Content for the first column */}
         <h4>Analogy Care</h4>
-        <img src={ac} style={{display: 'block', width: '750px', height: '500px', margin: 'auto' }}/>
+        <a href="https://analogycare.com/" target="_blank"><img src={ac} style={{display: 'block', width: '750px', height: '500px', margin: 'auto' }}/></a>
+        <p>First Column Content</p>
+      </div>
+      <div className="column">
+        {/* Content for the second column */}
+        <p>Second Column Content</p>
+      </div>
+</div>
+<Spacer h={30} />
+<div className="rowContainer">
+      <div className="column">
+        {/* Content for the first column */}
+        <h4>Realfun Learning Centre</h4>
+        <a href="https://realfun-portfolio-portal.web.app/" target="_blank"><img src={realfun} style={{display: 'block', width: '750px', height: '500px', margin: 'auto' }}/></a>
         <p>First Column Content</p>
       </div>
       <div className="column">
