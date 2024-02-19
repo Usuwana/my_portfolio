b62f891f562d03163ccaf140363af9d6a1a8e76c
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-19 20:24:07 +1100

    .

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,9 @@
+import './/styles.css'
+
 export const Footer  = () => {
     return (
         <footer style={{backgroundColor: '#333', color: 'aliceblue', paddingBottom: '0px', textAlign: 'center', position: 'fixed', bottom: 0, width: '100%' }}>
-        Developed by Tatenda Mapuranga using React Typescript.
+        Developed by Tatenda Mapuranga using React.
       </footer>
   
     )
