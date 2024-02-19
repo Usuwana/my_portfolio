c336b1dd4047db667e7a7d2fb484d96927ee4db6
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-20 01:42:12 +1100

    .

--- src/screens/contact/index.tsx
+++ src/screens/contact/index.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
 import { FindMe } from "../../components/findme"
 import { Footer } from "../../components/footer"
 import Forms from "../../components/form"
 import { Header } from "../../components/header"
 
 export const Contact  = () => {
+    useEffect(() => {
+        document.title = "Tatenda Mapuranga - Contact"
+      }, [])
     return (
         <div style={{ height: '100vh', width: '100vw', backgroundColor: '#1C1A1A'}}>
             <Header home={false} projects={false} contact={true}/>
--- src/screens/projects/index.tsx
+++ src/screens/projects/index.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
 import { FindMe } from "../../components/findme"
 import { Footer } from "../../components/footer"
 import { Header } from "../../components/header"
 import { ProjectsMain } from "../../components/projects_main"
 
 export const Projects  = () => {
+    useEffect(() => {
+        document.title = "Tatenda Mapuranga - Projects"
+      }, [])
     return (
         <div style={{height: '100vh', width: '100vw', backgroundColor: '#1C1A1A'}}>
             <Header home={false} projects={true} contact={false}/>
