cc93a99378e8ef5b77b14075dddd76317b682653
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-25 13:19:23 +1100

    .

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,17 @@
+import useWindowSize from '../../hooks/useWindowSize';
+import { Size } from '../../types';
 import './/styles.css'
 
 export const Footer  = () => {
-    return (
-        <footer style={{backgroundColor: '#333', color: 'aliceblue', paddingBottom: '0px', textAlign: 'center', position: 'fixed', bottom: 0, width: '100%' }}>
+  const size: Size = useWindowSize();
+    return size.width >= 900 ?(
+        <footer className="footer" style={{backgroundColor: '#333', color: 'aliceblue', paddingBottom: '0px', textAlign: 'center', position: 'fixed', bottom: 0, width: '100%' }}>
         Developed by Tatenda Mapuranga using React.
       </footer>
   
+    ) : (
+      <footer className="footer">
+      Developed by Tatenda Mapuranga using React.
+    </footer>
     )
 }
