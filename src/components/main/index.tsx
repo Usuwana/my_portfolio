ffbfea76917786c03017d8179b38e6bd828c8d56
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-29 01:24:02 +1100

    .

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -3,6 +3,7 @@ import profpic from './/49365479.png';
 import prof from './/prof.png';
 import react from './/react.png';
 import flutter from './/flutter.png';
+import gifImage from './/hello.gif';
 import java from './/java.png';
 import html from './/html.png';
 import css from './/css.png';
@@ -13,26 +14,14 @@ import { Size } from "../../types";
 import { useState } from "react";
 
 
-const profStyle = {
-    
-    //marginLeft: 50,
-    width: 300,
-    height: 300,
-    //alignSelf: 'center',
-    // Add more styles as needed
-  };
 
-const boxStyle = {
-    //alignSelf: 'center',
-    width: 600,
-    height: 200
-}
 
 
 export const Main  = () => {
   const listItems = [html, css, typescript, react, flutter, java];
   const listItems1 = [html, css, typescript];
   const listItems2 = [react, flutter, java];
+  const [isHovered, setIsHovered] = useState(false);
   const size: Size = useWindowSize();
     const [visible, setVisible] = useState(false);
     const showDrawer = () => {
@@ -47,10 +36,16 @@ export const Main  = () => {
            <div className="column-container" style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
               <Spacer h={30} />
               <div className="column" >
-              <img src={prof} style={{display: 'block', width: '350px', height: '200px', margin: 'auto' }}/>
+              <img src={prof} style={{display: 'blo6ck', width: '350px', height: '200px', margin: 'auto' }}/>
               </div>
               <div className="column" style={{textAlign: 'center', justifyContent: 'center', alignItems: 'center', padding: '10px' }}>
-              <p>Hello there! I'm Tatenda Mapuranga, a Cyber Security enthusiast currently mastering the art while holding a degree in Software Engineering. I thrive in the world of code as a Software Developer, always tweaking my web and mobile app development skills through exciting projects. I'm not just about coding; I've embraced the challenges of Full Stack development, and my journey involves balancing it all while pursuing my Cyber Security degree. Join me on this tech adventure – from coding marvels to securing the digital frontier!</p>
+              <p><span onMouseOver={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} style={{backgroundColor: '#333', cursor: 'pointer'}}>Hello there!</span>  {isHovered && (
+        <img
+          src={gifImage}
+          alt="GIF"
+          style={{ width: '200px', height: '200px', position: 'absolute' }} // Adjust the size accordingly
+        />
+      )}   I'm Tatenda Mapuranga, a Cyber Security enthusiast currently mastering the art while holding a degree in Software Engineering. I thrive in the world of code as a Software Developer, always tweaking my web and mobile app development skills through exciting projects. I'm not just about coding; I've embraced the challenges of Full Stack development, and my journey involves balancing it all while pursuing my Cyber Security degree. Join me on this tech adventure – from coding marvels to securing the digital frontier!</p>
               </div>
               <Spacer h={20} />
               <h3 style={{display: 'block', margin: 'auto' }}>Tech Stack:</h3>
@@ -106,7 +101,7 @@ export const Main  = () => {
               <img src={prof} style={{display: 'block', width: '350px', height: '200px', margin: 'auto' }}/>
               </div>
               <div className="column">
-              <p style={{display: 'block', margin: '20px'}}>My name is Tatenda Mapuranga. I am a Masters in Cyber Security student and I hold a degree in Software Engineering. I am a Software Developer. I am currently enhancing my web development skillset through projects and I have also done work in mobile application development. Through my projects and experiences in software development, I have done Full Stack development and I hope to keep working towards being a Full Stack Developer alongside working towards finishing my Cyber Security degree in working as a Cyber Security Analyst. </p>
+              <p style={{display: 'block', margin: '20px'}}>Hello there! I'm Tatenda Mapuranga, a Cyber Security enthusiast currently mastering the art while holding a degree in Software Engineering. I thrive in the world of code as a Software Developer, always tweaking my web and mobile app development skills through exciting projects. I'm not just about coding; I've embraced the challenges of Full Stack development, and my journey involves balancing it all while pursuing my Cyber Security degree. Join me on this tech adventure – from coding marvels to securing the digital frontier!</p>
               </div>
               <Spacer h={20} />
               <h3 style={{display: 'block', margin: 'auto' }}>Tech Stack:</h3>
@@ -137,7 +132,7 @@ export const Main  = () => {
               <img src={prof} style={{display: 'block', width: '350px', height: '200px', margin: 'auto' }}/>
               </div>
               <div className="column">
-              <p style={{display: 'block', margin: '20px'}}>My name is Tatenda Mapuranga. I am a Masters in Cyber Security student and I hold a degree in Software Engineering. I am a Software Developer. I am currently enhancing my web development skillset through projects and I have also done work in mobile application development. Through my projects and experiences in software development, I have done Full Stack development and I hope to keep working towards being a Full Stack Developer alongside working towards finishing my Cyber Security degree in working as a Cyber Security Analyst. </p>
+              <p style={{display: 'block', margin: '20px'}}>Hello there! I'm Tatenda Mapuranga, a Cyber Security enthusiast currently mastering the art while holding a degree in Software Engineering. I thrive in the world of code as a Software Developer, always tweaking my web and mobile app development skills through exciting projects. I'm not just about coding; I've embraced the challenges of Full Stack development, and my journey involves balancing it all while pursuing my Cyber Security degree. Join me on this tech adventure – from coding marvels to securing the digital frontier!</p>
               </div>
               <Spacer h={20} />
               <h3 style={{display: 'block', margin: 'auto' }}>Tech Stack:</h3>
